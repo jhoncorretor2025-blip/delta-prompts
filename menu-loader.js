bb17661f2f8dade7b017749e6bd7b79c531ba74b
jhoncorretor2025-blip <jhoncorretor2025@gmail.com>
2026-02-12 21:55:17 -0300

    Update menu-loader.js

--- menu-loader.js
+++ menu-loader.js
@@ -1,4 +1,8 @@
-fetch("menu.html")
+const path = window.location.pathname.includes("/paginas/")
+  ? "../menu.html"
+  : "menu.html";
+
+fetch(path)
   .then(res => res.text())
   .then(html => {
     document.getElementById("menu").innerHTML = html;
