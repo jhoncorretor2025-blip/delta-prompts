ba59c91392f63646fcc66e71e6f873d7a7aab369
jhoncorretor2025-blip <jhoncorretor2025@gmail.com>
2026-02-12 21:26:38 -0300

    Update menu-loader.js

--- menu-loader.js
+++ menu-loader.js
@@ -1,16 +1,5 @@
-const basePath = location.hostname.includes("github.io")
-  ? "/delta-prompts/"
-  : "/";
-
-fetch(basePath + "menu.html")
+fetch("menu.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("menu").innerHTML = html;
-
-    document.querySelectorAll("#menu a").forEach(link => {
-      const href = link.getAttribute("href");
-      if (!href.startsWith("http")) {
-        link.setAttribute("href", basePath + href);
-      }
-    });
   });
